ee63163a2617362b7d631b2fe0149131fbf4c93b
Kislichenko Bogdan <bogdan600@yandex.ru>
2018-12-22 22:18:17 +0300

    XML-XSD-validation

---
+++ src/main/java/ru/kislichenko/validation/MainValidate.java
@@ -0,0 +1,18 @@
+package ru.kislichenko.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Service: XML validation against XSD.
+ *
+ * @author bogdan
+ */
+@SpringBootApplication
+public class MainValidate {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainValidate.class, args);
+    }
+}
+
